e16dc50dbceaf160fb1f0bbfd3d02477637a6a2c
mr-Arturio <mr.arturio@gmail.com>
2025-03-18 11:54:18 -0400

    determin layout, add navigation + footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <nav>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li>
+              <Link href="#about">About</Link>
+            </li>
+            <li>
+              <Link href="#experience">Experience</Link>
+            </li>
+            <li>
+              <Link href="#projects">Projects</Link>
+            </li>
+            <li>
+              <Link href="#contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright &#169; 2023 Artur T. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { NavigationProps } from "../types";
+import { Theme } from "../constants/enums";
+
+const Navigation: React.FC<NavigationProps> = ({ toggleTheme, theme }) => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav id="desktop-nav">
+      {/* Desktop Navigation */}
+      <div className="logo">Artur T</div>
+      <div>
+        <ul className="nav-links">
+          <li>
+            <Link href="#about">About</Link>
+          </li>
+          <li>
+            <Link href="#experience">Experience</Link>
+          </li>
+          <li>
+            <Link href="#projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="#contact">Contact</Link>
+          </li>
+          <li>
+            <img
+              className="icon color-icon"
+              src={
+                theme === Theme.Dark
+                  ? "/assets/theme_dark.png"
+                  : "/assets/theme_light.png"
+              }
+              alt="Toggle Theme"
+              onClick={toggleTheme}
+            />
+          </li>
+        </ul>
+      </div>
+
+      {/* Mobile Navigation */}
+      <div id="hamburger-nav">
+        <div className="logo">Artur T</div>
+        <div className="hamburger-menu">
+          <div
+            className={`hamburger-icon ${menuOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <ul className={`menu-links ${menuOpen ? "open" : ""}`}>
+            <li>
+              <Link href="#about" onClick={toggleMenu}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="#experience" onClick={toggleMenu}>
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link href="#projects" onClick={toggleMenu}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link href="#contact" onClick={toggleMenu}>
+                Contact
+              </Link>
+            </li>
+            <li>
+              <img
+                className="icon color-icon"
+                src={
+                  theme === Theme.Dark
+                    ? "/assets/theme_dark.png"
+                    : "/assets/theme_light.png"
+                }
+                alt="Toggle Theme"
+                onClick={toggleTheme}
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
