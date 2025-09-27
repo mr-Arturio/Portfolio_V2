e7f38225fa62406b7b46f623511d609b55188afc
mr-Arturio <mr.arturio@gmail.com>
2025-09-26 23:38:04 -0400

    add publication

--- src/data/publicationsData.tsx
+++ src/data/publicationsData.tsx
@@ -1,9 +1,38 @@
 export const publicationsData = [
   {
+    imageSrc: "/assets/Sheks.jpeg",
+    altText: "Publication 5",
+    title: "Stop Waiting to Be Hired — Build Something That Matters",
+    description:
+      "How I turned a simple spreadsheet into a live Next.js app with real users, lessons, and metrics.",
+    mediumLink:
+      "https://medium.com/@mr.arturio/stop-waiting-to-be-hired-build-something-that-matters-b793a04bf407",
+  },
+  {
+    imageSrc: "/assets/Sheks.jpeg",
+    altText: "Publication 4",
+    title: "Finding Your People: The Power of Tech Community Across Canada",
+    description:
+      "In this article, I share how you can discover local and virtual tech groups, contribute meaningfully, turn conversations into career momentum...",
+    mediumLink:
+      "https://medium.com/@mr.arturio/finding-your-people-the-power-of-tech-community-across-canada-21aaaeb87cfe",
+  },
+  {
+    imageSrc: "/assets/Sheks.jpeg",
+    altText: "Publication 3",
+    title:
+      "Why I Chose Google Sheets as a Database for a Real-World Web App (And Why You Might Too)",
+    description:
+      "In this article, I share my experience using Google Sheets as a database for a non-profit social project, including pros, cons, and automation tips.",
+    mediumLink:
+      "https://medium.com/@mr.arturio/why-i-chose-google-sheets-as-a-database-for-a-real-world-web-app-and-why-you-might-too-4a25315cfc48",
+  },
+  {
     imageSrc: "/assets/Mem.jpeg",
     altText: "Publication 1",
     title: "Distinct nature of JavaScript",
-    description: "In this article, I explore some weird aspects of JavaScript that often leave developers scratching their heads.",
+    description:
+      "In this article, I explore some weird aspects of JavaScript that often leave developers scratching their heads.",
     mediumLink:
       "https://medium.com/@mr.arturio/distinct-nature-of-javascript-f4e9c3cc3f0f",
   },
@@ -12,26 +41,9 @@ export const publicationsData = [
     altText: "Publication 2",
     title:
       "To Mutate or Not to Mutate: The Shakespearean Dilemma of Array Methods",
-      description: "Exploring mutable vs immutable array operations in JavaScript.",
+    description:
+      "Exploring mutable vs immutable array operations in JavaScript.",
     mediumLink:
       "https://medium.com/@mr.arturio/to-mutate-or-not-to-mutate-the-shakespearean-dilemma-of-array-methods-4295f739e9b7",
-  },
-  {
-    imageSrc: "/assets/Sheks.jpeg",
-    altText: "Publication 3",
-    title:
-      "Why I Chose Google Sheets as a Database for a Real-World Web App (And Why You Might Too)",
-      description: "In this article, I share my experience using Google Sheets as a database for a non-profit social project, including pros, cons, and automation tips.",
-    mediumLink:
-      "https://medium.com/@mr.arturio/why-i-chose-google-sheets-as-a-database-for-a-real-world-web-app-and-why-you-might-too-4a25315cfc48",
-  },
-  {
-    imageSrc: "/assets/Sheks.jpeg",
-    altText: "Publication 4",
-    title:
-      "Finding Your People: The Power of Tech Community Across Canada",
-      description: "In this article, I share how you can discover local and virtual tech groups, contribute meaningfully, turn conversations into career momentu and some other tips of networking",
-    mediumLink:
-      "https://medium.com/@mr.arturio/finding-your-people-the-power-of-tech-community-across-canada-21aaaeb87cfe",
   },
 ];
